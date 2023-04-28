e20a78c0efef31ac73c551c269c328b006d45649
alex-r-gong <64893409+alex-r-gong@users.noreply.github.com>
2023-04-28 19:18:01 -0400

    create new search bar that returns brewery objects

--- app/src/main/java/com/example/breweryfinderapp/MainActivity.kt
+++ app/src/main/java/com/example/breweryfinderapp/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.breweryfinderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SearchView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    private final lateinit var brewerySearch: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        brewerySearch = findViewById(R.id.search_bar)
         getBrewery()
     }
 
@@ -25,7 +28,8 @@ class MainActivity : AppCompatActivity() {
                 response: String?,
                 throwable: Throwable?
             ) {
-                Log.d("Brewery Error", "error")
+                getBreweryNames(brewerySearch)
+                Log.d("Brewery","success! $response")
             }
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
@@ -34,4 +38,38 @@ class MainActivity : AppCompatActivity() {
 
         }]
     }
+
+    data class Brewery(
+        val id: Int,
+        val name: String,
+        val street: String?,
+        val city: String?,
+        val state: String?,
+        val postal_code: String?,
+        val country: String?,
+        val phone: String?,
+        val website_url: String?
+    )
+    private fun searchBreweries(query: String): List<Brewery> {
+        return searchBreweries(query)
+    }
+
+    private fun getBreweryNames(searchView: SearchView){
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+//                query?.let { searchBreweries(it) }?.forEach { brewery ->
+//                    println(brewery.name)
+//                }
+                val breweries = query?.let { searchBreweries(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                val breweries = query?.let { searchBreweries(it) }
+                return false
+            }
+        })
+    }
+
+
 }
