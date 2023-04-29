5cc47504446f0468db3e313d0a06d9b91efeb516
Sai-Krishna7 <sai7august@gmail.com>
2023-04-28 22:35:25 -0700

    Updated UI for Items in the Recycler View.

--- app/src/main/java/com/example/breweryfinderapp/MainActivity.kt
+++ app/src/main/java/com/example/breweryfinderapp/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.breweryfinderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var breweryList: MutableList<Triple<String,String,String>>
+    private lateinit var rvBreweries: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        breweryList = arrayListOf()
+        rvBreweries = findViewById(R.id.brewery_list)
 
         getBrewery()
     }
@@ -30,6 +38,23 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
                 Log.d("Brewery", "success! $json")
+
+                val array = json?.jsonArray
+
+                if (array != null) {
+                    for(i in 0 until array.length()) {
+                        val name = array.getJSONObject(i).getString("name")
+                        val street = array.getJSONObject(i).getString("street")
+                        val city = array.getJSONObject(i).getString("city")
+
+                        breweryList.add(Triple(name, street, city))
+                    }
+                    val adapter = BreweryAdapter(breweryList)
+                    rvBreweries.adapter = adapter
+                    rvBreweries.layoutManager = LinearLayoutManager(this@MainActivity)
+                    rvBreweries.addItemDecoration(DividerItemDecoration(this@MainActivity,LinearLayoutManager.VERTICAL))
+
+                }
             }
 
         }]
