efdbb39e810a607f8d0385f51e9836eade6f85f6
Sai-Krishna7 <sai7august@gmail.com>
2023-05-03 21:10:55 -0700

    Added a Home Page and Search Functionality works.

--- app/src/main/java/com/example/breweryfinderapp/Activity2.kt
+++ app/src/main/java/com/example/breweryfinderapp/Activity2.kt
@@ -1,11 +1,22 @@
 package com.example.breweryfinderapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Activity2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val button = findViewById<Button>(R.id.brewieButton)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
